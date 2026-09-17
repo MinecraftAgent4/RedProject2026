a19c4a8ad7260ae950c1a06193c1e0c90898c7f0
rguillau <guillaume.riti@ynov.com>
2026-09-17 15:46:41 +0200

    potion

--- personnage.go
+++ personnage.go
@@ -8,3 +8,24 @@ type Character struct {
 	pv_actuelle int
 	inventaire  []string
 }
+
+func (c *Character) AddPV(x int) {
+	c.pv_actuelle += x
+
+	if c.pv_actuelle > c.pv_total {
+		c.pv_actuelle = c.pv_total
+	}
+}
+
+func (c *Character) TakePot(p potion) {
+	for i, objet := range c.inventaire {
+		if objet == p.nom {
+			if p.effet != nil {
+				p.effet()
+			}
+
+			c.inventaire = append(c.inventaire[:i], c.inventaire[i+1:]...)
+			return
+	}
+}
+}
