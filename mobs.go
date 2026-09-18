5f24f7c00935f4848b6824c22b89e2b0d92c5f35
MinecraftAgent4 <nhautemer@gmail.com>
2026-09-18 14:50:01 +0200

    Entity Interface

--- mobs.go
+++ mobs.go
@@ -7,6 +7,20 @@ type Monster struct {
 	weapon Arme
 }
 
+type Entity interface {
+	Name() string
+	MaxHp() int
+	Hp() int
+}
+
+func (mob Monster) Name() string {return mob.name}
+func (mob Monster) MaxHp() int {return mob.pv_total}
+func (mob Monster) Hp() int {return mob.pv_actuelle}
+
+func (guy Character) Name() string {return guy.nom}
+func (guy Character) MaxHp() int {return guy.pv_total}
+func (guy Character) Hp() int {return guy.pv_actuelle}
+
 func initGoblin() Monster {
 	name := "Gobelin d'entraînement"
 	pv_total := 40
