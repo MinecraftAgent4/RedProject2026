41c00deb2faa5f5d5a3a18607b8e667b8f59041b
rguillau <guillaume.riti@ynov.com>
2026-09-21 15:16:38 +0200

    Ajoute un personnage sans saut

--- game.go
+++ game.go
@@ -19,14 +19,34 @@ func game() {
 		{rect: rl.Rectangle{X: 630, Y: 360, Width: 180, Height: 24}, color: rl.Green},
 		{rect: rl.Rectangle{X: 820, Y: 220, Width: 100, Height: 24}, color: rl.Green},
 	}
+	player := rl.Rectangle{X: 40, Y: 408, Width: 32, Height: 42}
+	playerSpeed := float32(220)
 
 	for !rl.WindowShouldClose() {
+		delta := rl.GetFrameTime()
+		if rl.IsKeyDown(rl.KeyRight) || rl.IsKeyDown(rl.KeyD) {
+			player.X += playerSpeed * delta
+		}
+		if rl.IsKeyDown(rl.KeyLeft) || rl.IsKeyDown(rl.KeyA) {
+			player.X -= playerSpeed * delta
+		}
+		if player.X < 0 {
+			player.X = 0
+		}
+		if player.X+player.Width > 960 {
+			player.X = 960 - player.Width
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.NewColor(28, 37, 61, 255))
 		for _, block := range blocks {
 			rl.DrawRectangleRec(block.rect, block.color)
 			rl.DrawRectangleLinesEx(block.rect, 2, rl.DarkGray)
 		}
+		rl.DrawRectangleRec(player, rl.SkyBlue)
+		rl.DrawCircle(int32(player.X+10), int32(player.Y+13), 3, rl.Black)
+		rl.DrawCircle(int32(player.X+22), int32(player.Y+13), 3, rl.Black)
+		rl.DrawText("A/D ou fleches : se deplacer", 24, 20, 18, rl.RayWhite)
 		rl.EndDrawing()
 	}
 }
