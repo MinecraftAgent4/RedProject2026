30ec077d513f8fe2633c3ae7e839c218ca5580b8
rguillau <guillaume.riti@ynov.com>
2026-09-21 15:11:05 +0200

    Ajoute une map de plateformes en rectangles

--- game.go
+++ game.go
@@ -2,15 +2,31 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+type mapBlock struct {
+	rect  rl.Rectangle
+	color rl.Color
+}
+
 func game() {
 	rl.InitWindow(960, 540, "RedProject")
 	rl.SetTargetFPS(60)
 	defer rl.CloseWindow()
 
+	blocks := []mapBlock{
+		{rect: rl.Rectangle{X: 0, Y: 450, Width: 960, Height: 90}, color: rl.DarkGreen},
+		{rect: rl.Rectangle{X: 80, Y: 360, Width: 180, Height: 24}, color: rl.Green},
+		{rect: rl.Rectangle{X: 350, Y: 290, Width: 180, Height: 24}, color: rl.Green},
+		{rect: rl.Rectangle{X: 630, Y: 360, Width: 180, Height: 24}, color: rl.Green},
+		{rect: rl.Rectangle{X: 820, Y: 220, Width: 100, Height: 24}, color: rl.Green},
+	}
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.DarkBlue)
-		rl.DrawText("RedProject", 380, 240, 40, rl.RayWhite)
+		rl.ClearBackground(rl.NewColor(28, 37, 61, 255))
+		for _, block := range blocks {
+			rl.DrawRectangleRec(block.rect, block.color)
+			rl.DrawRectangleLinesEx(block.rect, 2, rl.DarkGray)
+		}
 		rl.EndDrawing()
 	}
 }
